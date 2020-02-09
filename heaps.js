37a998716c057a3c64de17b7d568d3e53cc8c051
James Clark <james.s.clark12@gmail.com>
2020-02-09 18:16:13 +0000

    Update heaps.js

--- heaps.js
+++ heaps.js
@@ -69,6 +69,10 @@ const MinHeapify = (array, i, heapSize = array.length) => {
 // Only calling the heapify operation of the first half of the does make sense when you think of the array
 // structure - but should consider the mathematical definition of what proporition of the array needs to be
 // called on in order to ensure that the comparisons are complete
+
+// Heapify is called moving backwards going through the array because the comparisons it does are only downwards
+// .i.e. towards its children. so going downwards from the start of the array would not allow a max value stored
+// at leaf level to make it's way all the way to the top - working backwards does allow this
 const BuildMinHeap = (array) => {
   for (let i=Math.ceil(array.length/2); i>-1; i--) {
     MinHeapify(array, i);
